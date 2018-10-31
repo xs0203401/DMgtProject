793e080e5fe501485779fea981ae29f9fb503cc0
xs0203401 <yaohuang.liu@utexas.edu>
2018-10-31 00:59:51 -0500

    database name

--- CreditMe/CreditMe/settings.py
+++ CreditMe/CreditMe/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
         # 'ENGINE': 'django.db.backends.mysql' instead of the following.
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'CreditMe',
+        'NAME': 'dmgtproject:us-central1:dmgt-mysql',
         'USER': 'root',
         'PASSWORD': 'LYH0527lyh',
         'HOST': '35.193.119.93',
