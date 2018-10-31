d25944a9e146a3906da4b25c233844bc9d200ddd
xs0203401 <yaohuang.liu@utexas.edu>
2018-10-31 00:31:53 -0500

    database

--- CreditMe/CreditMe/settings.py
+++ CreditMe/CreditMe/settings.py
@@ -75,7 +75,6 @@ WSGI_APPLICATION = 'CreditMe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'default': {
         # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
         # 'ENGINE': 'django.db.backends.mysql' instead of the following.
         'ENGINE': 'django.db.backends.mysql',
