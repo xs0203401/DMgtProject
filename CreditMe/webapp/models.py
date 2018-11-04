7992b93303d7abb3282a4b929d78bc4fd225dba2
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-04 03:53:21 -0600

    models

--- CreditMe/webapp/models.py
+++ CreditMe/webapp/models.py
@@ -1,3 +1,30 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    point_recd = models.IntegerField(default=0)
+    point_tosd = models.IntegerField(default=1000)
+    username = models.CharField(max_length=16)
+    password = models.CharField(max_length=128)
+
+
+class Transaction(models.Model):
+	rec_ID = models.ForeignKey(Employee)
+	send_ID = models.ForeignKey(Employee)
+	points = models.IntegerField(default=0)
+	message = models.ForeignKey(Message)
+	rdm_ID = model.ForeignKey(Redemption)
+	pub_date = models.DateTimeField('transaction date stamp')
+
+class Redemption(models.Model):
+	point_price = models.IntegerField(default=100)
+	title = models.CharField(max_length=64)
+
+class Message(models.Model):
+	title = models.CharField(max_length=80)
+	content = models.CharField(max_length=240)
