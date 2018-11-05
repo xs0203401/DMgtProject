46b1863e16153202deb1fb55a198404a95b78f84
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-04 20:44:39 -0600

    none

--- CreditMe/webapp/views.py
+++ CreditMe/webapp/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 import datetime
 
 # Create your views here.
-@login_required(redirect_field_name='login/')
+@login_required(login_url='login/')
 def index(request):
 	return render(request, 'webapp/home.html')
 
