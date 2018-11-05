a1aa5bf222b8cb553da3e278673f057a56648306
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-04 22:45:59 -0600

    auth

--- CreditMe/webapp/urls.py
+++ CreditMe/webapp/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('login/', views.login, name='login'),
+	path('login/', auth_views.LoginView.as_view()),
 ]
