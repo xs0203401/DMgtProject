891db5b714c9c173e6533f276bec4d9098293604
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-04 23:53:19 -0600

    home

--- CreditMe/webapp/urls.py
+++ CreditMe/webapp/urls.py
@@ -6,4 +6,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('login/', auth_views.LoginView.as_view()),
+	path('logout/', views.logout_view, name='logout'),
 ]
--- CreditMe/webapp/views.py
+++ CreditMe/webapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 from .models import Employee, Redemption, Message, Transaction
 
 import datetime
@@ -17,3 +19,6 @@ def index(request):
     }
 	return render(request, 'webapp/home.html', context)
 
+def logout_view(request):
+    logout(request)
+    return redirect('/')
