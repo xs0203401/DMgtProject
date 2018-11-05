0f52866286c373725d93c2436e2c92dcd69f6929
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-05 00:31:52 -0600

    home

--- CreditMe/webapp/views.py
+++ CreditMe/webapp/views.py
@@ -13,14 +13,14 @@ def logout_view(request):
     logout(request)
     return redirect('/')
 
-def get_this_user_employee():
+def get_this_user_employee(request):
 	this_user = request.user
 	this_employee = Employee.objects.get(user_id=this_user)
 	return this_user, this_employee
 
 @login_required(login_url='login/')
 def index(request):
-	this_user, this_employee = get_this_user_employee()
+	this_user, this_employee = get_this_user_employee(request)
 
 	context = {
         'user': this_user,
@@ -32,7 +32,7 @@ def index(request):
 
 @login_required(login_url='login/')
 def send(request):
-	this_user, this_employee = get_this_user_employee()
+	this_user, this_employee = get_this_user_employee(request)
 	employees = Employee.objects.exclude(pk=e1.id)
 
 
