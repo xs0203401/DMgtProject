09d30523e4443955401094aec64fbbad8a9cc3f1
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-04 23:01:24 -0600

    home

--- CreditMe/webapp/views.py
+++ CreditMe/webapp/views.py
@@ -5,7 +5,15 @@ from django.contrib.auth.decorators import login_required
 import datetime
 
 # Create your views here.
-@login_required(login_url='login/')
+@login_required()
 def index(request):
+	this_user = request.user
+
+	this_employee = 
+
+	context = {
+        'question': question,
+        'error_message': "You didn't select a choice.",
+    }
 	return render(request, 'webapp/home.html')
 
