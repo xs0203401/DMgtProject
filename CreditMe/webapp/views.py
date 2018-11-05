c984e501db62a1cfcbb319be2977a86984cd0ecc
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-04 23:11:49 -0600

    home

--- CreditMe/webapp/views.py
+++ CreditMe/webapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Employee, Redemption, Message, Transaction
 
 import datetime
 
@@ -8,8 +9,8 @@ import datetime
 @login_required(login_url='login/')
 def index(request):
 	this_user = request.user
-	print(this_user)
-	# this_employee = 
+	print(this_user.id)
+	this_employee = Employee.
 
 	context = {
         # 'question': question,
