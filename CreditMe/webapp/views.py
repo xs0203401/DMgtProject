5bed976f204165cf60e067350c8fb168023d96a0
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-04 18:24:29 -0600

    change_path

--- CreditMe/webapp/views.py
+++ CreditMe/webapp/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def index(request):
+	return render(request, 'webapp/home.html')
+
+def login(request):
 	return render(request, 'webapp/login.html')
