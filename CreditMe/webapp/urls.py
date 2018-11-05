5143b2fe991826e1c8b3ff6d278d5b99e33442e5
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-05 00:30:15 -0600

    home

--- CreditMe/webapp/urls.py
+++ CreditMe/webapp/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('send/', views.send, name='send')
 	path('login/', auth_views.LoginView.as_view()),
 	path('logout/', views.logout_view, name='logout'),
 ]
--- CreditMe/webapp/views.py
+++ CreditMe/webapp/views.py
@@ -9,10 +9,18 @@ from .models import Employee, Redemption, Message, Transaction
 from datetime import datetime
 
 # Create your views here.
-@login_required(login_url='login/')
-def index(request):
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+def get_this_user_employee():
 	this_user = request.user
 	this_employee = Employee.objects.get(user_id=this_user)
+	return this_user, this_employee
+
+@login_required(login_url='login/')
+def index(request):
+	this_user, this_employee = get_this_user_employee()
 
 	context = {
         'user': this_user,
@@ -21,6 +29,17 @@ def index(request):
     }
 	return render(request, 'webapp/home.html', context)
 
-def logout_view(request):
-    logout(request)
-    return redirect('/')
+
+@login_required(login_url='login/')
+def send(request):
+	this_user, this_employee = get_this_user_employee()
+	employees = Employee.objects.exclude(pk=e1.id)
+
+
+	context = {
+        'user': this_user,
+        'employee': this_employee,
+        'employees': employees,
+        'datetime': timezone.now().date(),
+    }
+	return render(request, 'webapp/send.html', context)
