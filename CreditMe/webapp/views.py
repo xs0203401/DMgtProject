77d2ef36d26da78c59e6a1eeddac4110927c1dc1
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-06 16:25:47 -0600

    refine_business_logics

--- CreditMe/webapp/views.py
+++ CreditMe/webapp/views.py
@@ -9,6 +9,11 @@ from django.contrib.auth.models import User
 
 from datetime import datetime
 
+TRANS_STATUS={
+	'SUCCESS':0,
+	'ILLEGAL':1,
+	'INSUFFICIENT':2
+}
 
 def get_this_user_employee(request):
 	this_user = request.user
@@ -38,27 +43,61 @@ def send(request):
 		# <QueryDict: {'msg_content': ['ccc'], 'csrfmiddlewaretoken': ['JDs57HliW4SDzNaW2KOqjSDOupRxQ9
 		# duv2kmqNER4GzLDg7JoLkJVYlmebSMlafT'], 'msg_title': ['yttt'], 'points': ['15'], 'rec_user': [
 		# 'henryliu']}>
-		# print(request.POST)
-		rec_user = User.objects.get(pk=int(request.POST['rec_user']))
-		rec_employee = Employee.objects.get(user_id=rec_user)
+		
+		# get receiver and sender
 		this_user = request.user
 		send_employee = Employee.objects.get(user_id=this_user)
-		this_trans=Transaction(
-			rec_ID=rec_employee,
-			send_ID=send_employee,
-			points=int(request.POST['points']),
-			pub_date=timezone.now()
+		try:
+			rec_user = User.objects.get(pk=int(request.POST['rec_user']))
+			rec_employee = Employee.objects.get(user_id=rec_user)
+		except:
+			return redirect('/send?status={}'.format(TRANS_STATUS['ILLEGAL']))
+
+		# get transaction
+		try:
+			trans_points = int(request.POST['points'])
+		except:
+			return redirect('/send?status={}'.format(TRANS_STATUS['ILLEGAL']))
+
+		if trans_points <= 0:
+			return redirect('/send?status={}'.format(TRANS_STATUS['ILLEGAL']))
+
+		# check if point sufficient
+		if send_employee.point_tosd <= trans_points:
+			return redirect('/send?status={}'.format(TRANS_STATUS['INSUFFICIENT']))
+
+		# transaction is legal
+		# Record transaction
+		this_trans = Transaction(
+			rec_ID = rec_employee,
+			send_ID = send_employee,
+			points = trans_points,
+			pub_date = timezone.now()
 			)
+
+		# if there is message information save message
 		if request.POST['msg_title']!='' or request.POST['msg_content']!='':
+			# create and save message
 			trans_msg = Message(
-				title=str(request.POST['msg_title']),
-				content=str(request.POST['msg_content'])
+				title = str(request.POST['msg_title']),
+				content = str(request.POST['msg_content'])
 				)
 			trans_msg.save()
-			this_trans.message=trans_msg
+			# key transaction
+			this_trans.message = trans_msg
+		# change sender points
+		send_employee.point_tosd -= trans_points
+		# change receiver points
+		rec_employee.point_recd += trans_points
+		
+		# Close transaction
+		# save transaction record
 		this_trans.save()
-		trans_status = True
-		return redirect('/send?done={}'.format(trans_status))
+		send_employee.save()
+		rec_employee.save()
+
+		# return success status
+		return redirect('/send?status={}'.format(TRANS_STATUS['SUCCESS']))
 
 	elif request.method == 'GET':
 
@@ -81,21 +120,43 @@ def redemption(request):
 		# <QueryDict: {'rdp_id': ['1'], 'csrfmiddlewaretoken': ['bWtXhAUY4nNf3WT7ZWLX3QpTRZM9439bXll
 		# eAGdxcZun7pQUlXhgFW7rBLNoz4bA']}>
 		# print(request.POST)
+
+		# get system employee
 		sys_employee = Employee.objects.get(pk=6) # system pk=6
+		
+		# get this user
 		this_user = request.user
 		this_employee = Employee.objects.get(user_id=this_user)
-		rdp_item = Redemption.objects.get(pk=int(request.POST['rdp_id']))
+		
+		# get redemption item
+		try:
+			rdp_item = Redemption.objects.get(pk=int(request.POST['rdp_id']))
+		except:
+			return redirect('/send?status={}'.format(TRANS_STATUS['ILLEGAL']))
+		trans_points = rdp_item.point_price
+
+		# check if point sufficient
+		if this_employee.point_recd <= trans_points:
+			return redirect('/send?status={}'.format(TRANS_STATUS['INSUFFICIENT']))
+
+		# transaction is legal
+		# Record transaction
 		this_trans=Transaction(
 			rec_ID=sys_employee,
 			send_ID=this_employee,
-			points=int(rdp_item.point_price),
+			points=trans_points,
 			rdm_ID=rdp_item,
 			pub_date=timezone.now()
 			)
-		this_trans.save()
+		this_employee.point_recd -= trans_points
 
-		trans_status = True
-		return redirect('/redemption?done={}'.format(trans_status))
+		# Close transaction
+		# save objects
+		this_trans.save()
+		this_employee.save()
+
+		# return success status
+		return redirect('/redemption?status={}'.format(TRANS_STATUS['SUCCESS']))
 
 	elif request.method == 'GET':
 
