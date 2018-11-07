025b758d1beeb25ec7b73e8b7c8c4625dc7ef28a
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-07 03:27:00 -0600

    report_url

--- CreditMe/webapp/urls.py
+++ CreditMe/webapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('send/', views.send, name='send'),
 	path('redemption/', views.redemption, name='redemption'),
-	path('report/<int:report_id>', views.report, name='report_page')
+	path('report/<int:report_id>', views.report, name='report_page'),
 	path('login/', auth_views.LoginView.as_view()),
 	path('logout/', views.logout_view, name='logout'),
 ]
