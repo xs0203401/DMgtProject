a9f62c9c1cf043e68fcca356d74153f07b1e47e8
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-07 12:16:43 -0600

    report_ready

--- CreditMe/webapp/views.py
+++ CreditMe/webapp/views.py
@@ -74,7 +74,7 @@ def report(request, report_id):
 	# NEED to be tested!
 	# sql_s = str(this_report.sql_string).lower()
 	sql_s = str(this_report.sql_string)
-	print("SQL:",sql_s)
+	print("SQL execute:",sql_s)
 	
 	# try to get report column names,
 	# otherwise, no header
@@ -88,6 +88,7 @@ def report(request, report_id):
 	with connection.cursor() as csr:
 		csr.execute(sql_s)
 		query_result = csr.fetchall()
+		# print(query_result)
 		report_cols = [dcp[0] for dcp in csr.description]
 
 	context = {
@@ -95,6 +96,7 @@ def report(request, report_id):
         'employee': this_employee,
         'datetime': timezone.now().date(),
         'report_list': report_list,
+        'report_sql': sql_s,
         'report_title': this_report.title,
         'report_header': report_cols,
         'report_content':query_result,
