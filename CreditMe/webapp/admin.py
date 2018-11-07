7467591133b5d510bdd0ca4189eaea33c8ab45d7
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-07 03:24:28 -0600

    admin.py_Report

--- CreditMe/webapp/admin.py
+++ CreditMe/webapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Employee, Redemption, Message, Transaction
+from .models import Employee, Redemption, Message, Transaction, Report
 
 # Register your models here.
 admin.site.register(Employee)
 admin.site.register(Redemption)
 admin.site.register(Message)
 admin.site.register(Transaction)
+admin.site.register(Report)
