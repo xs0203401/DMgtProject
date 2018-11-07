8420749bacd9342909c3ad4a8e76c35aec83eb0c
xs0203401 <yaohuang.liu@utexas.edu>
2018-11-07 12:57:57 -0600

    gen_random

--- CreditMe/webapp/gen_random.py
+++ CreditMe/webapp/gen_random.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 import random
 
-def generate_data(n=20, num_month=1, num_month_ago=0, s_r_percent=0.80, point_per=0.3):
+def generate_data(n=20, num_month=1, num_month_ago=0, s_r_percent=0.9, point_per=0.3):
 	'''
 	n = rows of data
 	num_month = number of month(s) in range (default = 1)
@@ -14,7 +14,6 @@ def generate_data(n=20, num_month=1, num_month_ago=0, s_r_percent=0.80, point_pe
 	s_r_percent = percent of sending (vs. redemption)
 	point_per = the random range of points
 
-	this generator does not change points in employees' accounts
 	'''
 	num_month=int(num_month)
 	num_month_ago=int(num_month_ago)
@@ -52,12 +51,18 @@ def generate_data(n=20, num_month=1, num_month_ago=0, s_r_percent=0.80, point_pe
 			)
 		else:
 			e_send = employees[int(random.random()*len(employees))]
+			this_rdm = None
+			for rdm in rdm_s:
+				if rdm.point_price<e_send.point_recd:
+					this_rdm = rdm
+					break
+			if this_rdm is None: next
+			
 			this_trans = Transaction(
 				rec_ID = sys_employee,
 				send_ID = e_send,
 				pub_date = timezone.now()-timedelta(days=int(random.random()*30*num_month+1+30*num_month_ago))
 				)
-			this_rdm = rdm_s[int(random.random()*len(rdm_s))]
 			this_trans.rdm_ID = this_rdm
 			this_trans.points = this_rdm.point_price
 			rnd_msg = Message(
